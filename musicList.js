c25dc99a3ab49b39e8361a334ad6045897a35e9b
Muhammad iar Hossain <72465649+Md-Iar-Hossain@users.noreply.github.com>
2024-01-08 03:53:22 +0000

    I changed audioTrackObject to Array

--- musicList.js
+++ musicList.js
@@ -1,78 +1,78 @@
 'user strict'
-const audioTrackObject = {
-  '0': {
+const audioTrackObject = [
+  {
     trackTitle: "The Way of The Tears - Exclusive Nasheed - Muhammad al Muqit",
     img: 'img/1.jpg',
     url: 'audios/The Way of The Tears - Exclusive Nasheed - Muhammad al Muqit.mp3'
   },
-  '1': {
+  {
     trackTitle: "Badr Ud Duja - Junaid Jamshed",
     img: 'img/3.jpg',
     url: 'audios/Badr Ud Duja - Junaid Jamshed.mp3'
   },
-  '2': {
+  {
     trackTitle: "The_Beauty_of_Existence_-_Heart_Touching_Nasheed",
     img: 'img/5.jpg',
     url: 'audios/The_Beauty_of_Existence_-_Heart_Touching_Nasheed.mp3'
   },
-  '3': {
+  {
     trackTitle: "Mohabbat kya hai - Junaid Jamshed",
     img: 'img/6.jpg',
     url: 'audios/Mohabbat kya hai - Junaid Jamshed.mp3'
   },
-  '4': {
+  {
     trackTitle: "Ya_Ilahi_-_Powerful_Nasheed_By_Ishaq_Ayubi",
     img: 'img/7.jpg',
     url: 'audios/Ya_Ilahi_-_Powerful_Nasheed_By_Ishaq_Ayubi.mp3'
   },
-  '5': {
+  {
     trackTitle: "Ilahi Teri Chaukhat Per_Junaid Jamshed",
     img: 'img/8.jpg',
     url: 'audios/Junaid Jamshed Heart Touching Naat - Ilahi Teri Chaukhat Per.mp3'
   },
-  '6': {
+  {
     trackTitle: "Emotional_Arabic_Nasheed!",
-    img: "img/9.jpg",  
+    img: "img/9.jpg",
     url: 'audios/Emotional_Arabic_Nasheed!!_you_can_not_stop_your_tears.mp3'
   },
-  '7': {
+  {
     trackTitle: "Ae Nabi Pyare Nabi",
-    img: "img/10.jpg",  
+    img: "img/10.jpg",
     url: 'audios/Ae Nabi Pyare Nabi.mp3'
   },
-  '8': {
+  {
     trackTitle: "HUMOOD - KUN ANTA",
-    img: "img/2.jpg", 
+    img: "img/2.jpg",
     url: 'audios/HUMOOD - KUN ANTA (كن أنت) - [VOCALS ONLY   SLOWED   REVERB].mp3'
   },
-  '9': {
+  {
     trackTitle: "Aey Rasool E Amin Tujh Sa Koi Nahin - Junaid Jamshed",
-    img: "img/11.jpg", 
+    img: "img/11.jpg",
     url: 'audios/Aey Rasool E Amin Tujh Sa Koi Nahin - Junaid Jamshed.mp3'
   },
-  '10': {
+  {
     trackTitle: "Enchanting_poems_of_Allama_Iqbal",
     img: 'img/12.jpg',
     url: 'audios/Enchanting_poems_of_Allama_Iqbal.mp3'
   },
-  '11': {
+  {
     trackTitle: "Muhammad Ka Roza Naat by Junaid Jamshed",
     img: 'img/13.jpg',
     url: 'audios/Muhammad Ka Roza Naat by Junaid Jamshed.mp3'
   },
-  '12': {
+  {
     trackTitle: "Mera dil badal de _ Junaid Jamshed",
     img: 'img/4.jpg',
     url: 'audios/Mera dil badal de _ Junaid Jamshed.mp3'
   },
-  '13': {
+  {
     trackTitle: "NAAT_ Jaane Janaan _ Ayaan Sajad  _ Arif Mir",
     img: 'img/14.jpg',
     url: 'audios/NAAT_ Jaane Janaan _ Ayaan Sajad  _ Arif Mir _ Xehran Films _ Rj Umar Nisar.mp3'
   },
-  '14': {
+  {
     trackTitle: "Tamanna e Dil _Junaid Jamshed.mp3",
     img: 'img/15.jpg',
     url: 'audios/Tamanna e Dil _Junaid Jamshed.mp3'
   }
-}
+]
